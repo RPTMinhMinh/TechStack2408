d0f2dbbbe17d5ead28b5d2b2d48cfe12f4f6d191
Minh Minh <ngnhatminh71203@gmail.com>
2024-09-24 16:44:38 +0700

    update homework

---
+++ MODULE 2/LESSON 1/CLASS/script.js
@@ -0,0 +1,78 @@
+// alert('Js hahaa')
+
+// var x;
+// var y = 10;
+// console.log(y);
+
+// let a;
+// let b =200;
+// console.log(b);
+
+// const PI = 3.14;
+// console.log(PI);
+
+// x= 22;
+// console.log(x);
+// x =12;
+// console.log(x);
+// x =78
+// console.log(x);
+
+
+// let w =10;
+// let e =20;
+// let r ='30';
+
+// let res1 = w +e;
+// console.log(res1);
+
+// let res2 = res1 + r;
+// console.log(res2);
+
+
+// let num1 = 13;
+// let num2 = 2;
+// let result;
+
+// console.log(num1 + num2);   //15
+     
+// console.log(num1 - num2);   //11
+
+// console.log(num1 * num2);   //26
+// console.log(num1 / num2);   //6.5
+// // chia lay du
+// console.log(num1 % num2);   //1
+// //luy thua
+// console.log(num1 ** num2);  //13^2 = 13 * 13 = 169
+
+// result = result + num1;     //result += num1
+// result = result - num1;     //result -= num1
+
+// let word1 = 'hello';
+// let word2 = 'world';
+// let greeting = word1 + ' ' +word2;
+
+// console.log(greeting);
+
+// console.log(10 == '10');    //true
+// console.log(10 === '10')    //false
+
+// let abc = 10;
+// let xyz = '5';
+// let jqk = 2;
+// console.log(abc + xyz);
+// console.log(abc -xyz);
+// console.log(abc +xyz -jqk);
+// console.log(xyz - abc);
+
+
+// let a = 11;
+// let b = '22';
+
+// // let c = a - (b++);
+// // console.log(c);
+
+// let d = a - (++b);
+// console.log(d);
+
+let 
